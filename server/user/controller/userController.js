3649dbfde286b49baf68da2632c8fe04aa525f1a
Steven Moyano <stevenmoyano1@gmail.com>
2021-09-20 17:16:31 -0500

    worked and completed update user controller function

--- server/user/controller/userController.js
+++ server/user/controller/userController.js
@@ -48,12 +48,10 @@ async function login(req, res) {
       let comparedPassword = await bcrypt.compare(password, foundUser.password);
 
       if (!comparedPassword) {
-        res
-          .status(400)
-          .json({
-            message: "failure",
-            payload: "Please check your email and password",
-          });
+        res.status(400).json({
+          message: "failure",
+          payload: "Please check your email and password",
+        });
       } else {
         let jwtToken = jwt.sign(
           {
@@ -70,5 +68,30 @@ async function login(req, res) {
     }
   } catch (e) {
     res.json({ message: "error", error: e });
+  }
+}
+
+async function updateUser(req, res, next) {
+  if (req.body.password) {
+    let salt = await bcrypt.genSalt(12);
+    let hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    req.body.password = hashedPassword;
+  }
+
+  try {
+    let updatedUser = await User.findOneAndUpdate(
+      { email: res.locals.decodedJwt.email },
+      req.body,
+      { new: true }
+    );
+
+    if (req.body.password) {
+      res.status(200).json({ message: "Success", payload: updatedUser });
+    } else {
+      res.json({ message: "Success", payload: updatedUser });
+    }
+  } catch (e) {
+    next(e);
   }
 }
