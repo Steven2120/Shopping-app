18c634e8d7be1dea0d4561a8b4b2d618ad1ee07b
Steven Moyano <stevenmoyano1@gmail.com>
2021-09-19 19:06:04 -0500

    added numerous dependencies to assist with authentication and began setting up signup controller

--- server/user/controller/userController.js
+++ server/user/controller/userController.js
@@ -1,1 +1,30 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 const User = require("../model/User");
+
+async function signup(req, res, next) {
+  const { username, email, password, firstName, lastName } = req.body;
+  const { errorObj } = res.locals;
+
+  if (Object.keys(errorObj).length > 0) {
+    return res.status(500).json({ message: "failure", payload: errorObj });
+  }
+
+  try {
+    let salt = await bcrypt.genSalt(12);
+    let hashedPassword = await bcrypt.hash(password, salt);
+
+    const createdUser = new User({
+      firstName,
+      lastName,
+      email,
+      username,
+      password: hashedPassword,
+    });
+
+    let savedUser = await createdUser.save();
+    res.json({ message: "success", data: savedUser });
+  } catch (e) {
+    next(e);
+  }
+}
