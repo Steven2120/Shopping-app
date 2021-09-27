158343a0e1d06ceb0d02bd0ba3935913067252d0
Steven Moyano <stevenmoyano1@gmail.com>
2021-09-27 17:43:54 -0500

    added helper functions

--- server/user/helpers/authMiddleware.js
+++ server/user/helpers/authMiddleware.js
@@ -1,7 +1,44 @@
 const {
-  checkIsEmpty,
-  checkIsStrongPassword,
   checkIsEmail,
   checkIsAlpha,
   checkIsAlphaNumeric,
 } = require("../utils/authMethods");
+
+function checkIsEmailFunc(req, res, next) {
+  const { errorObj } = res.locals;
+
+  if (!checkIsEmail(req.body.email)) {
+    errorObj.wrongEmailFormat = "Must be in email format!";
+  }
+  next();
+}
+
+function checkIsAlphaFunc(req, res, next) {
+  const { errorObj } = res.locals;
+
+  const inComingData = req.body;
+
+  for (key in inComingData) {
+    if (key === "firstName" || key === "lastName") {
+      if (!checkIsAlpha(inComingData[key])) {
+        errorObj[`${key}`] = `${key} can only have characters`;
+      }
+    }
+  }
+  next();
+}
+
+function checkIsAlphaNumericFunc(req, res, next) {
+  const { errorObj } = res.locals;
+
+  if (!checkIsAlphaNumeric(!req.body.username)) {
+    errorObj.usernameError = "Username can only have characters and numbers";
+  }
+  next();
+}
+
+module.exports = {
+  checkIsEmailFunc,
+  checkIsAlphaFunc,
+  checkIsAlphaNumericFunc,
+};
---
+++ server/user/helpers/checkIsEmptyFunc.js
@@ -0,0 +1,16 @@
+const { checkIsEmpty } = require("../utils/authMethods");
+
+function checkIsEmptyFunc(req, res, next) {
+  const { errorObj } = res.locals;
+
+  let inComingData = req.body;
+
+  for (let key in inComingData) {
+    if (checkIsEmpty(inComingData[key])) {
+      errorObj[key] = `${key} cannot be empty`;
+    }
+  }
+  next();
+}
+
+module.export = checkIsEmptyFunc;
